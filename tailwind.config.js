142fb3bf9517597792fd5cfc137bab4e2b933b85
Mohammad Almosallam <moh.almusallam4@gmail.com>
2024-02-09 21:32:40 +0300

    Theme config & Components initialization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -16,15 +17,78 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    fontFamily: {
+      sans: ["IBM Plex Sans Arabic", "sans-serif"],
+    },
     extend: {
       colors: {
+        gray: {
+          900: "#101828",
+          800: "#1D2939",
+          700: "#344054",
+          600: "#475467",
+          500: "#667085",
+          400: "#98A2B3",
+          300: "#D0D5DD",
+          200: "#EAECF0",
+          100: "#F2F4F7",
+          50: "#F9FAFB",
+          25: "#FCFCFD",
+        },
+        success: {
+          900: "#054f31",
+          800: "#05603a",
+          700: "#027a48",
+          600: "#039855",
+          500: "#12b76a",
+          400: "#32d583",
+          300: "#6ce9a6",
+          200: "#a6f4c5",
+          100: "#d1fadf",
+          50: "#ecfdf3",
+          25: "#f6fef9",
+        },
+        warning: {
+          900: "#7a2e0e",
+          800: "#93370d",
+          700: "#b54708",
+          600: "#dc6803",
+          500: "#f79009",
+          400: "#fdb022",
+          300: "#fec84b",
+          200: "#fedf89",
+          100: "#fef0c7",
+          50: "#fffaeb",
+          25: "#fffcf5",
+        },
+        error: {
+          900: "#7a271a",
+          800: "#912018",
+          700: "#b42318",
+          600: "#d92d20",
+          500: "#f04438",
+          400: "#f97066",
+          300: "#fda29b",
+          200: "#fecdca",
+          100: "#fee4e2",
+          50: "#fef3f2",
+          25: "#fffbfa",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
+          900: "#134E4A",
+          800: "#115E59",
+          700: "#0F766E",
           DEFAULT: "hsl(var(--primary))",
+          500: "#14B8A6",
+          400: "#2DD4BF",
+          300: "#5EEAD4",
+          200: "#99F6E4",
+          100: "#CCFBF1",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
@@ -73,5 +137,6 @@ module.exports = {
       },
     },
   },
+
   plugins: [require("tailwindcss-animate")],
-}
+};
