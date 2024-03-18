6d70e2eba046880e417142565bd5e93fdbc9e6fc
Majed1945 <majedbiazid@gmail.com>
2024-03-18 08:40:07 +0300

    newFeat

--- src/features/patients/pages/Patients.jsx
+++ src/features/patients/pages/Patients.jsx
@@ -32,6 +32,7 @@ const Patients = () => {
         <StatisticsBox />
         <StatisticsBox />
         <StatisticsBox />
+        <StatisticsBox />
       </section>
       <section className="flex flex-col gap-6 ">
         <div className="flex gap-4 w-fit">
